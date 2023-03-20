61103b37a6ddabc64d6f5a776f4b42da55d6cd5d
Nilanjan2111 <128204965+Nilanjan2111@users.noreply.github.com>
2023-03-20 10:14:40 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My parents healthy new Diner')
+
+streamlit.subtitle('Breakfast Menu')
+streamlit.body('Omega 3 & Blueberry Oatmeal')
+
+streamlit.body('Kale, Spinack & Rocket Smoothie')
+
+streamlit.body('Hard-Boiled Free-Range Egg')
+
